1d59a42996ec366c95e7814498e33d18e973c473
S.Alireza Ezaz <S.Alireza Ezaz@SALIREZA-EZAZ>
2021-02-01 19:05:57 +0330

    Added advertisement properties.

---
+++ BackEnd/OnlineStockShop/Domain/Category/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStockShop.Domain.Category
+{
+    public class Category
+    {
+
+    }
+}

833ff1cfd572f23b169c73a0d754abaf1e5f97e0
S.Alireza Ezaz <S.Alireza Ezaz@SALIREZA-EZAZ>
2021-02-01 19:14:09 +0330

    Added user model.

--- BackEnd/OnlineStockShop/Domain/User/User.cs
+++ BackEnd/OnlineStockShop/Domain/User/User.cs
@@ -7,39 +7,26 @@ using System.Threading.Tasks;
 
 namespace OnlineStockShop.Domain.User
 {
-    [Table ("User")]
+
     public class User
     {
         #region properties
-        [Display(Name = "ایمیل")]
-        [Required(ErrorMessage = "لطفا  را وارد کنید")]
-        [MaxLength(100, ErrorMessage = "تعداد کاراکتر های {0} نمی تواند بیشتر از {1} باشد")]
+
+        public byte Id { get; set; }
+
         public string Email { get; set; }
 
-        [Display(Name = "کلمه ی عبور")]
-        [Required(ErrorMessage = "لطفا  را وارد کنید")]
-        [MaxLength(100, ErrorMessage = "تعداد کاراکتر های {0} نمی تواند بیشتر از {1} باشد")]
         public string Password { get; set; }
 
-        [Display(Name = "نام")]
-        [Required(ErrorMessage = "لطفا  را وارد کنید")]
-        [MaxLength(100, ErrorMessage = "تعداد کاراکتر های {0} نمی تواند بیشتر از {1} باشد")]
         public string FirstName { get; set; }
 
-        [Display(Name = "نام خانوادگی")]
-        [Required(ErrorMessage = "لطفا  را وارد کنید")]
-        [MaxLength(100, ErrorMessage = "تعداد کاراکتر های {0} نمی تواند بیشتر از {1} باشد")]
         public string LastName { get; set; }
 
-
-        [MaxLength(100, ErrorMessage = "تعداد کاراکتر های {0} نمی تواند بیشتر از {1} باشد")]
-        public string EmailActiveCode { get; set; }
-        public bool IsActivated { get; set; }
 
         #endregion
 
         #region Relations
-        //public ICollection<Advertisment> UserRoles { get; set; }
+        //public ICollection<Advertisment> Users { get; set; }
         #endregion
 
 

5dad50330a49a5fb1493e6befd0fb5aca37cb192
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2021-02-01 21:13:34 +0330

    Changed home component location

--- FrontEnd/OnlineStockShop/src/app/app-routing.module.ts
+++ FrontEnd/OnlineStockShop/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './Pages/home/home.component';
 
 const routes: Routes = [
-  // {path: 'signup', component: SignUpComponent}
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- FrontEnd/OnlineStockShop/src/app/app.module.ts
+++ FrontEnd/OnlineStockShop/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HomeComponent} from './Home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {TooltipModule} from 'ngx-bootstrap/tooltip';
 import {ModalModule} from 'ngx-bootstrap/modal';
+import {HomeComponent} from './Pages/home/home.component';
 
 
 

3a19ef80c5cecebf272bc0a5d5b2400fb3aee271
2arian3 <aryan.boukani2@gmail.com>
2021-02-03 20:51:47 +0330

    changed title in report

--- BackEnd/OnlineStockShop/Domain/Report/Report.cs
+++ BackEnd/OnlineStockShop/Domain/Report/Report.cs
@@ -8,14 +8,10 @@ namespace OnlineStockShop.Domain.Report
 {
     public class Report
     {
-
         public byte Id { get; set; }
         public string Description { get; set; }
         public string Title { get; set; }
-
-
         public byte AdvertismentId { get; set; }
-
         public virtual Advertisement Advertisment { get; set; }
     }
 }

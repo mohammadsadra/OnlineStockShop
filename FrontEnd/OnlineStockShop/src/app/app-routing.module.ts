d5032f2e10370a0dcd7b0121a878b99dcc62e336
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2021-02-04 19:39:59 +0330

    Creat new addNewComponent

--- FrontEnd/OnlineStockShop/src/app/app-routing.module.ts
+++ FrontEnd/OnlineStockShop/src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './Pages/home/home.component';
 import {AdvertismentsComponent} from './Pages/advertisments/advertisments.component';
-import {CreateAdComponent} from "./Pages/create-ad/create-ad.component";
+import {NewAdvertisementComponent} from './Pages/new-advertisement/new-advertisement.component';
+
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', component: HomeComponent},
   {path: 'advertisments', component: AdvertismentsComponent},
-  {path: 'create-ad', component: CreateAdComponent}
+  {path: 'newAd', component: NewAdvertisementComponent}
 ];
 
 @NgModule({
--- FrontEnd/OnlineStockShop/src/app/app.module.ts
+++ FrontEnd/OnlineStockShop/src/app/app.module.ts
@@ -11,13 +11,13 @@ import {AdvertismentsComponent, DialogContentExampleDialog} from './Pages/advert
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatDialogModule} from '@angular/material/dialog';
 import {HttpClientModule} from '@angular/common/http';
-import { CreateAdComponent } from './Pages/create-ad/create-ad.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatExpansionModule} from "@angular/material/expansion";
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MatIconModule} from "@angular/material/icon";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
 import {MatNativeDateModule} from '@angular/material/core';
+import { NewAdvertisementComponent } from './Pages/new-advertisement/new-advertisement.component';
 
 
 
@@ -28,7 +28,7 @@ import {MatNativeDateModule} from '@angular/material/core';
     HomeComponent,
     AdvertismentsComponent,
     DialogContentExampleDialog,
-    CreateAdComponent
+    NewAdvertisementComponent
   ],
   imports: [
     BrowserModule,
